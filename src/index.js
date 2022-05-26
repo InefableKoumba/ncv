f9b9fff3afed1e50ed442a34a5b972df8013db87
InefableKoumba <76625159+InefableKoumba@users.noreply.github.com>
2022-05-26 13:04:05 +0100

    update

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
     <MantineProvider
       theme={{
         // Override any other properties from default theme
-        fontFamily: 'Roboto, Open Sans, sans serif',
+        fontFamily: 'Roboto, Open Sans',
         spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
       }}
     >
